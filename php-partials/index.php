823418aebccd312d4bcf09aeed2e23b1e7d13c69
Shayne <shaynemcgregor1@gmail.com>
2021-11-19 07:52:47 -0500

    add php partial challenge

---
+++ php-partials/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+
+			<title>[[enter title]]</title>
+			<meta name="description" content="[[insert description]]">
+			<meta property="og:image" content="images/default-meta.png">
+
+			<link rel="stylesheet" href="css/style.css">
+		</head>
+
+
+		<body>
+			<?php include("header.php"); ?>
+			<div>I'm an Intro</div>
+			<div>I'm a Call to Action</div>
+			<?php include("footer.php"); ?>
+		</body>
+	</html>
